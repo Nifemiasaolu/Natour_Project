735b7557122f614c36cbf20fae60eb7d99bd32d2
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-05-24 13:43:28 +0100

    Details added

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({
   path: "./config.env",
 });
-const app = require("./app");
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected successfully!"));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Tour Name is required"],
+    unique: true,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+
+  price: {
+    type: Number,
+    required: [true, "Tour Price is required"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
