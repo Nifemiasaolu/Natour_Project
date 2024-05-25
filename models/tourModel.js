f6be593b932e49144f7e27a2f9cdeb9bafdf3c30
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-05-25 19:56:59 +0100

    Tweak on details

---
+++ models/tourModel.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Tour Name is required"],
+    unique: true,
+    trim: true,
+  },
+
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, "Tour must have a group size"],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
+  price: {
+    type: Number,
+    required: [true, "Tour Price is required"],
+  },
+
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "Tour summary required"],
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, "Tour cover image is required"],
+  },
+
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+module.exports = Tour;
