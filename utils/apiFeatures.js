ed494101f719206b3ea9d8da445bc58441e5bbe6
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-05-27 20:09:34 +0100

    Tweak on details

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -58,3 +58,55 @@ class APIFeatures {
 }
 
 module.exports = APIFeatures;
+
+///////////////////////////////////////////
+// BUILD QUERY
+
+// 1A) FILTERING Query
+// FIltering works with the find method.
+// const queryObj = { ...req.query };
+// const excludedFields = ["page", "sort", "limit", "fields"];
+// excludedFields.forEach((el) => delete queryObj[el]);
+
+// // 1B) ADVANCED FILTERING (Bringing the gte, gt, lte & lt method into consideration)
+// let queryString = JSON.stringify(queryObj);
+// queryString = queryString.replace(
+//   /\b(gte|gt|lte|lt)\b/g,
+//   (match) => `$${match}`,
+// );
+
+// const queryStr = JSON.parse(queryString);
+
+// let query = Tour.find(queryStr); // First Method of writing a/filter query
+
+// 2) SORTING
+// // Sorting works with the sort method.
+// if (req.query.sort) {
+//   const sortBy = req.query.sort.split(",").join(" ");
+//   query = query.sort(sortBy);
+// } else {
+//   query = query.sort("-createdAt");
+// }
+
+// 3) FIELD LIMITING
+// Limiting field (display certain data) works with the select method.
+// if (req.query.fields) {
+//   const fields = req.query.fields.split(",").join(" ");
+//   query = query.select(fields);
+// } else {
+//   query = query.select("-__v");
+// }
+
+// 4) PAGINATION
+// Pagination works with the skip and limit method.
+
+// const page = req.query.page * 1 || 1;
+// const limit = req.query.limit * 1 || 100;
+// const skip = (page - 1) * limit;
+
+// query = query.skip(skip).limit(limit);
+
+// if (req.query.page) {
+//   const numTours = await Tour.countDocuments();
+//   if (skip >= numTours) throw new Error("Page Does not Exist!");
+// }
