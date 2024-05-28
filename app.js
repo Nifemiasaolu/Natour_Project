895b46a14f363ca0a9044825d3d53a5d25592eae
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-05-28 17:20:54 +0100

    Database middleware

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 // app.get("/api/v1/tours/:id", getTour);
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
-//////////////////////////////////////\\
+//////////////////////////////////////\
 
 // MIDDLEWARE FOR TOUR AND USER ROUTES
 // Mounting Routers
