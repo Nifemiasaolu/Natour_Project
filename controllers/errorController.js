2aae93df2e624b116689929a1cd17d866216fe4e
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-06-02 19:10:03 +0100

    Error Handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,56 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
+const AppError = require("../utils/appError");
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
+    error: err,
+    stack: err.stack,
   });
 };
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming, or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("Error💥", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong",
+    });
+  }
+};
+
+const globalErrorHandler = (err, req, res, next) => {
+  //   console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
+};
+
+module.exports = globalErrorHandler;
