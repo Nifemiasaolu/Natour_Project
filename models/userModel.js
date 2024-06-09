17c89b502a80eb9d20a0a285af296a697f2356ff
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-06-09 18:41:16 +0100

    Authorization Implementation

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const schema = {
     type: String,
     required: [true, "Please provide a password."],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -40,13 +41,22 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   // Run this function if password was actually modified(saved or updated)
-  // Hash the password with cost of 12
+  // Hash/Encrypt the password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
 
   // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
+
+// This is an instance method that returns True/False, while comparing the input password to the existing password.
+// An instance method is available on all the documents
+userSchema.methods.correctPassword = function (
+  candidatePassword,
+  userPassword,
+) {
+  return bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 
