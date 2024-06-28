310ad8ec6edca275bdef1670fca6068c0b00d898
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-06-28 14:46:14 +0100

    User Update

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,6 +52,14 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: "Success",
+    data: null,
+  });
+});
+
 // const getAllUsers = (req, res) => {
 //   res.status(500).json({
 //     status: "error",
