6e3c2ca36835799cb5a4ac3251751c1fe821b3dd
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-07-13 20:18:49 +0100

    Populating Reviews

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,7 +33,7 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id);
+  const tour = await Tour.findById(req.params.id).populate("reviews"); // Populates review for just a particular tour
   // Tour.findOne({_id: req.params.id}) // Shorthand of the above method
 
   if (!tour) {
