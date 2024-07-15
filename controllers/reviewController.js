4f547e4a01c1daca96cf446d6bcbc071c497dbbb
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-07-15 17:07:01 +0100

    Factory Function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,7 @@
 const Review = require("../models/reviewModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const factory = require("./handlerFactory");
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
@@ -22,24 +23,30 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  // Nested Routes
-  // This gets the body and tour automatically, if the user doesn't input them as part of the review.
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
+exports.createReview = factory.createOne(Review);
 
-  const newReview = await Review.create(req.body);
+exports.updateReview = factory.updateOne(Review);
 
-  if (!newReview) {
-    return next(new AppError("No review created"), 400);
-  }
+exports.deleteReview = factory.deleteOne(Review);
 
-  res.status(201).json({
-    status: "Success",
-    data: {
-      review: newReview,
-    },
-  });
-});
+// exports.createReview = catchAsync(async (req, res, next) => {
+//   // Nested Routes
+//   // This gets the body and tour automatically, if the user doesn't input them as part of the review.
+//   if (!req.body.tour) req.body.tour = req.params.tourId;
+//   if (!req.body.user) req.body.user = req.user.id;
+//
+//   const newReview = await Review.create(req.body);
+//
+//   if (!newReview) {
+//     return next(new AppError("No review created"), 400);
+//   }
+//
+//   res.status(201).json({
+//     status: "Success",
+//     data: {
+//       review: newReview,
+//     },
+//   });
+// });
 
 //////
