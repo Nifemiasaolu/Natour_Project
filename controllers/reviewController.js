27d7a1cb2e479724c7ee03fb99fec39736fa4772
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-07-16 20:57:10 +0100

    Factory Function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,33 +1,43 @@
 const Review = require("../models/reviewModel");
-const AppError = require("../utils/appError");
-const catchAsync = require("../utils/catchAsync");
+// const AppError = require("../utils/appError");
+// const catchAsync = require("../utils/catchAsync");
 const factory = require("./handlerFactory");
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  // GET Nested routes
-  if (req.params.tourId) filter = { tour: req.params.tourId };
+exports.setUserTourIds = (req, res, next) => {
+  // Nested Routes
+  // This gets the body and tour automatically, if the user doesn't input them as part of the review.
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const reviews = await Review.find(filter);
-
-  if (!reviews) {
-    return next(new AppError("No review found", 404));
-  }
-
-  res.status(200).json({
-    status: "Success",
-    result: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
-
 exports.updateReview = factory.updateOne(Review);
-
 exports.deleteReview = factory.deleteOne(Review);
+
+//////////////////////////////////////////////////////////////////
+// exports.getAllReviews = catchAsync(async (req, res, next) => {
+//   let filter = {};
+//   // GET Nested routes
+//   // This gets the tour automatically, if the user doesn't input it as part of the review.
+//   if (req.params.tourId) filter = { tour: req.params.tourId };
+//
+//   const reviews = await Review.find(filter);
+//
+//   if (!reviews) {
+//     return next(new AppError("No review found", 404));
+//   }
+//
+//   res.status(200).json({
+//     status: "Success",
+//     result: reviews.length,
+//     data: {
+//       reviews,
+//     },
+//   });
+// });
 
 // exports.createReview = catchAsync(async (req, res, next) => {
 //   // Nested Routes
