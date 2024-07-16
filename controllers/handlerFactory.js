1d8816ca8ffd9fd7571958e351011d6221379583
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-07-16 20:57:01 +0100

    Factory Function

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,20 +1,66 @@
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
-// const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour(hack).
+    let filter = {};
+    // This gets the tour automatically, if the user doesn't input it as part of the review.
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    // EXECUTE QUERY
+    // query.find().sort().select().skip().limit() //Wat The query method is now
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: "success",
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    // const doc = await Model.findById(req.params.id).populate("reviews");
+
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    });
+  });
 
 exports.createOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    if (Model === "Review") {
-      if (!req.body.tour) req.body.tour = req.params.tourId;
-      if (!req.body.user) req.body.user = req.user.id;
-    }
+    // if (Model === "Review") {
+    //   if (!req.body.tour) req.body.tour = req.params.tourId;
+    //   if (!req.body.user) req.body.user = req.user.id;
+    // }
 
     const doc = await Model.create(req.body);
 
     res.status(201).json({
       status: "success",
       data: {
-        tour: doc,
+        data: doc,
       },
     });
   });
