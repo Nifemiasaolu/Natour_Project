a13953b85fd0df15ac0608d910c9eb44cc7c8183
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-08-19 18:47:38 +0100

    Geospatial Implementation

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use((req, res, next) => {
   next();
 });
 
-/////////////////////////////////
+////////////////////////////////
 // app.get("/api/v1/tours", getAllTours);
 // app.post("/api/v1/tours", createTour);
 // app.get("/api/v1/tours/:id", getTour);
