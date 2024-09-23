6ac6b7f025d6fd3d7959292292fb7660663a1843
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-09-23 19:04:25 +0100

    Geospatial Implementation

--- models/tourModel.js
+++ models/tourModel.js
@@ -43,7 +43,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5.0"],
-      // set: val => Math.round(val * 10) / 10 // Rounds a 4.666666 to 4.7
+      set: val => Math.round(val * 10) / 10 // Rounds a 4.666666 to 4.7
     },
 
     ratingsQuantity: {
@@ -145,6 +145,7 @@ tourSchema.index({ price: 1, ratingsAverage: -1 });
 // 1 => Ascending
 // -1 => Descending
 tourSchema.index({ slug: 1 });
+tourSchema.index({startLocation: "2dsphere"})
 
 // VIRTUAL PROPERTY
 tourSchema.virtual("durationWeeks").get(function () {
