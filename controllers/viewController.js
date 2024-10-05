594400f3c5381da775cc4c9e0a69bfbaf0b3fbbf
Nifemiasaolu <theophilusasaolu@gmail.com>
2024-10-05 20:20:11 +0100

    Pug implementation

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,31 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('overview', {
-    title: "All tours"
-  })
-}
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
 
-exports.getTourDetail = (req, res) => {
-  res.status(200).render('tour', {
-    title: "The Forest Hiker Tour"
-  })
-}
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get all tours
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render the tours data from step 1
+
+  res.status(200).render("overview", {
+    title: "All tours",
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    field: "user rating review",
+  });
+  // .populate("guides");
+  // 2) Build template
+  // 3) Render template using data from 1)
+
+  res.status(200).render("tour", {
+    title: `${tour.name}`,
+    tour,
+  });
+});
